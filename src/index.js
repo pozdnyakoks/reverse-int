be32abadcb2093b625012693e6ae6e52d050d8a3
pozdnyakoks <pozdnyakoks@mail.ru>
2022-06-23 13:32:02 +0300

    change index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let newNumber = n.toString()
+    let array = [];
+    
+    for (let i = newNumber.length - 1; i >= 0; i--) {
+      array.push(newNumber[i]);
+    }
+    let numberReverse = array.join('').split('-').join('');
+
+    return Number(numberReverse);
 }
